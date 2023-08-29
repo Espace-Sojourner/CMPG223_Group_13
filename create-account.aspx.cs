957fa78a2e5e59639288514a3e20d045afa6d158
berna <berna@DESKTOP-RPQVB76>
2023-08-29 16:09:34 +0200

    Added gradient background

--- create-account.aspx.cs
+++ create-account.aspx.cs
@@ -9,9 +9,9 @@ namespace CMPG223_Group_13
 {
     public partial class createAccount : System.Web.UI.Page
     {
-        public static string Username = tbUsername.Text, User_Password = tbPassword.Text, User_Password = tbConfirmPassword.Text;
-        public static string First_Name = tbFirstname.Text, Last_Name = tbLastname.Text, Email_Address = tbEmail.Text, Phone_Number = tbPhone;
-        public static string Farm_Name = tbFarmName.Text;
+        //public static string Username = tbUsername.Text, User_Password = tbPassword.Text, User_Password = tbConfirmPassword.Text;
+       //public static string First_Name = tbFirstname.Text, Last_Name = tbLastname.Text, Email_Address = tbEmail.Text, Phone_Number = tbPhone;
+        //public static string Farm_Name = tbFarmName.Text;
 
         protected void Page_Load(object sender, EventArgs e)
         {
