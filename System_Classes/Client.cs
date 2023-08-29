51f87d4c2e6bed0b622db8663ff47ddb03b50b85
RoyaleNite <waldre321@gmail.com>
2023-08-29 14:38:56 +0200

    Added getting Farmer

--- System_Classes/Client.cs
+++ System_Classes/Client.cs
@@ -4,13 +4,15 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 namespace CMPG223_Group_13
 {
     public enum ClientType
     {
         Client,
-        Admin
+        Admin,
+        Farmer
     }
 
     class Client
@@ -40,8 +42,62 @@ namespace CMPG223_Group_13
         public static Client getByID(int ID)
         {
             string sql = $"SELECT * FROM Client WHERE Client_ID = {ID}";
-            DataTable dt =  DatabaseHandler.executeSelectToDT(sql);
-            return new Client((int)dt.Rows[0]["Client_ID"], (ClientType)dt.Rows[0]["Client_Type"], dt.Rows[0]["First_Name"].ToString(), dt.Rows[0]["Last_Name"].ToString(), dt.Rows[0]["Email_Address"].ToString(), dt.Rows[0]["Phone_Number"].ToString(), dt.Rows[0]["Shipping_Address"].ToString(), dt.Rows[0]["Password"].ToString());
+            DataTable dt = DatabaseHandler.executeSelectToDT(sql);
+            if (dt.Rows.Count == 0)
+            {
+                sql = $"SELECT * FROM Farmers WHERE Farmer_ID = {ID}";
+                dt = DatabaseHandler.executeSelectToDT(sql);
+                if (dt.Rows.Count == 0)
+                {
+                    return null;
+                }
+                else return RowToFarmer(dt.Rows[0]);
+            }
+            else return RowToClient(dt.Rows[0]);
+        }
+
+        /*public static Client getByEmail(string Email)
+        {
+            string sql = $"SELECT * FROM Client WHERE Email_Address = {Email}";
+            DataTable dt = DatabaseHandler.executeSelectToDT(sql);
+            if (dt.Rows.Count == 0) {
+                sql = $"SELECT * FROM Farmers WHERE Email_Address = {Email}";
+                dt = DatabaseHandler.executeSelectToDT(sql);
+                if (dt.Rows.Count == 0)
+                {
+                    return null;
+                }
+                else return RowToClient(dt.Rows[0]);
+            }            
+            else return RowToClient(dt.Rows[0]); 
+        }*/
+
+        public static Client getClientByEmail(string Email)
+        {
+            string sql = $"SELECT * FROM Client WHERE Email_Address = {Email}";
+            DataTable dt = DatabaseHandler.executeSelectToDT(sql);
+            if (dt.Rows.Count == 0) return null;
+            else return RowToClient(dt.Rows[0]);
+        }
+
+        public static Client getFarmerByEmail(string Email)
+        {
+            string sql = $"SELECT * FROM Farmer WHERE Email_Address = {Email}";
+            DataTable dt = DatabaseHandler.executeSelectToDT(sql);
+            if (dt.Rows.Count == 0)
+            {
+                return null;
+            }
+            else return RowToClient(dt.Rows[0]);
+        }
+
+        public static Client RowToClient(DataRow Row)
+        {
+            return new Client((int)Row["Client_ID"], (ClientType)Row["Client_Type"], Row["First_Name"].ToString(), Row["Last_Name"].ToString(), Row["Email_Address"].ToString(), Row["Phone_Number"].ToString(), Row["Shipping_Address"].ToString(), Row["Password"].ToString());
+        }
+        public static Client RowToFarmer(DataRow Row)
+        {
+            return new Client((int)Row["Farmer_ID"], ClientType.Farmer, Row["First_Name"].ToString(), Row["Last_Name"].ToString(), Row["Email_Address"].ToString(), Row["Phone_Number"].ToString(), null, Row["Password"].ToString());
         }
     }
 }
