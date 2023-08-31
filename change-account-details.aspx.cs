75b32290bde315031b958ed0ae7e8bf46f4e469d
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2023-08-31 09:01:56 +0200

    Added functionality
    Pending interface and class changes.
    Added functionality to update bank and farm info.

--- change-account-details.aspx.cs
+++ change-account-details.aspx.cs
@@ -12,70 +12,92 @@ namespace CMPG223_Group_13
     public partial class change_account_details : System.Web.UI.Page
     {
         User user;
+        Bank_Account_Info bank;
+        Farm farm;
+        private bool isFarmer = false;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            // --- test user "logged in" ---
-            //user = new User(12, UserType.Client, "User", "Resu", "user@gmail.com", "0722602483", "2 Home Street", "frikkieja");
-
+            // get all user details
             user = getSessionUser();
+            bank = Bank_Account_Info.getByID(user.User_ID);
+            if (user.UserType == UserType.Farmer)
+            {
+                isFarmer = true;
+                farm = Farm.getByID(user.User_ID);
+            }
 
-            //prefill user data
+            prefillData();
+            
+        }
+
+        // NOTE: could possibly add the below method to the User class to be accessed by all forms
+        protected User getSessionUser()
+        {
+            //returns user if logged in otherwise redirects to login
+            if (Session["User"] == null) Response.Redirect("~/default.aspx");
+            return (User)Session["User"];            
+        }
+
+        protected void prefillData()
+        {
+            // user data
             tbFirstname.Text = user.First_Name;
             tbLastname.Text = user.Last_Name;
             tbEmail.Text = user.Email_Address;
             tbPhone.Text = user.Phone_Number;
+            // PENDING: tbAddress.text = user.Shipping_Address;
+            // PENDING: tbPassword.text = user.User_Password;
 
-        }
+            // bank info
+            // PENDING: tbBankName.text = bank.Bank_Name;
+            // PENDING: tbAccNumber.text = bank.Account_Number;
 
-        //could possibly add the below method to the User class to be accessed by all forms
-        protected User getSessionUser()
-        {          
-            //returns user if logged in otherwise redirects to login
-            if (Session["User"] != null) return (User)Session["User"];                            
-            else
-            {
-                Response.Redirect("~/default.aspx");
-                return null; //won't execute but needed
-            }              
+            // farm info
+            // PENDING: tbfarmName.Text = (isFarmer) ? farm.Farm_Name : "N/A";
+            // PENDING: tbfarmAddress.Text = (isFarmer) ? farm.Farm_Address : "N/A";
         }
 
         protected void btnConfirm_Click(object sender, EventArgs e)
         {
-            /*
-             * TODO:
-             * - entries
-             * -- validate entries (validation controls)
-             * - controls
-             * -- require shipping address and password fields
-             * -- require farm fields
-             * -- require bank fields
-            */
+            // TODO: add required input controls & validation controls
 
-            //entries
+            // --- entries ---
+            // user details
             string firstName = tbFirstname.Text,
                 lastName = tbLastname.Text,
                 email = tbEmail.Text,
-                phone = tbPhone.Text;
+                phone = tbPhone.Text,
+                address = "", //PENDING: tbAddress.text
+                password = ""; //PENDING: tbPassword.text
+            // bank info
+            string bankName = "", //PENDING: tbBankName.text
+                accNumber = ""; //PENDING: tbAccNumber.text
+            // farm info - will be "N/A" if not farmer
+            string farmName = "", //PENDING: tbFarmName.text
+                farmAddress = ""; //PENDING: tbFarmAddress.text
 
-            //create updated user
-            User updated = new User(user.User_ID,user.UserType,firstName,lastName,email,phone,user.Shipping_Address,user.User_Password);
+            // create updated objects
+            User newUser = new User(user.User_ID,user.UserType,firstName,lastName,email,phone,address,password);
+            Bank_Account_Info newBank = new Bank_Account_Info(bank.Bank_Account_ID, (isFarmer) ? user.User_ID : -1, (isFarmer) ? -1 : user.User_ID,bankName,accNumber); // adapt bank class 
+            Farm newFarm = (isFarmer) ? new Farm(farm.Farm_ID,user.User_ID,farmName,farmAddress) : null;
 
-            //update user
-            switch (user.UserType)
+            if (isFarmer)
             {
-                case UserType.Admin:
-                case UserType.Client:
-                    updateClient(updated);
-                    //update Session
-                    user = getClientByID(user.User_ID);
-                    break;
-                case UserType.Farmer:
-                    updateFarmer(updated);
-                    //update Session
-                    user = getFarmerByID(user.User_ID);
-                    break;
+                updateFarmer(newUser);
+                // PENDING: Farm.updateFarm(newFarm);
+                user = getFarmerByID(user.User_ID); // update Session
             }
+            else
+            {
+                updateClient(newUser);
+                user = getClientByID(user.User_ID); // update Session
+            }
+
+            // PENDING: if (user.UserType != UserType.Admin) Bank_Account_Info.updateBank(newBank);
+
+            // send to dashboard
+            Response.Redirect("~/dashboard.aspx");
         }
     }
 }
