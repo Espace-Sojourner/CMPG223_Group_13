09eaedf21fc57a0442172e29b667de8e1c17e6ea
RoyaleNite <waldre321@gmail.com>
2023-09-02 12:04:17 +0200

    Created list-produce form and added code

--- list-produce.aspx.cs
+++ list-produce.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Resources;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static CMPG223_Group_13.DatabaseHandler;
 
 namespace CMPG223_Group_13
 {
@@ -11,7 +14,26 @@ namespace CMPG223_Group_13
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            LoadGV("Select * FROM Produce", "Produce", ref gvProduce);
+        }
 
+        protected void gvProduce_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Produce selProduce = Produce.RowToData(gvProduce.SelectedRow);
+            if (File.Exists(selProduce.Image_Link))
+            {
+                imgProduce.ImageUrl = selProduce.Image_Link;
+            }
+            else
+            {
+                imgProduce.ImageUrl = "./Resources/ProduceImages/Default.png";
+            }
+        }
+
+        protected void tbSubSearch_TextChanged(object sender, EventArgs e)
+        {
+            string sql = $"Select * FROM Produce WHERE Produce_Name LIKE '%{tbSubSearch.Text}%'";
+            LoadGV(sql, "Produce", ref gvProduce);
         }
     }
 }
