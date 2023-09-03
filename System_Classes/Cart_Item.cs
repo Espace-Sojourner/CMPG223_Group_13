bf336d3cb670d077235bfbdd582e83ddd9de0a5c
Ewan <108713859+M1NDLE55@users.noreply.github.com>
2023-09-03 08:40:14 +0200

    Minor fixes

--- System_Classes/Cart_Item.cs
+++ System_Classes/Cart_Item.cs
@@ -12,20 +12,20 @@ namespace CMPG223_Group_13.System_Classes
         public string produceName { get; set; }
         public int quantity { get; set; }
         public double price { get; set; }
-        public int UOM_ID { get; set; }
-        public Cart_Item(int LP_ID, string produceName, int quantity, int UOM_ID, double price) 
+        public string UOM { get; set; }
+        public Cart_Item(int LP_ID, string produceName, int quantity, string UOM, double price) 
         {
             this.LP_ID = LP_ID;
             this.produceName = produceName;
             this.quantity = quantity; // Quantity to purchase
-            this.UOM_ID = UOM_ID;
+            this.UOM = UOM;
             this.price = price; // Calculated price based on quantity
         }
 
         override
         public string ToString()
         {
-            return $"{produceName} {quantity}{Unit_of_Measure.getByID(UOM_ID)} {price:C}";
+            return $"{produceName} {quantity}{UOM} {price:C}";
         }
     }
 }
